ebaafcadb1400e95a46b9ff0b369ba0740a40662
David Thorpe <djt@mutablelogic.com>
2021-09-29 10:59:53 +0200

    Updated to remove internal docker service

--- start.sh
+++ start.sh
@@ -9,25 +9,20 @@ if [ "${ACCESS_TOKEN}" == "" ] ; then
     exit -1
 fi
 
-# Start docker
-service docker start || exit -1
-
 # Get the token
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
 # Configure the runner
 cd /home/docker/actions-runner
-gosu docker ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
+./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
 
 cleanup() {
     echo "Removing runner..."
-    gosu docker ./config.sh remove --unattended --token ${REG_TOKEN}
-    echo "Stopping docker..."
-    service docker stop
+    ./config.sh remove --unattended --token ${REG_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
 # Run the runner
-gosu docker ./run.sh & wait $!
+./run.sh & wait $!
