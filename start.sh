7d5478f3d185c822cb384ee6ee9c0caca9a97573
David Thorpe <djt@mutablelogic.com>
2021-09-29 10:10:44 +0200

    Start docker service

--- start.sh
+++ start.sh
@@ -9,6 +9,9 @@ if [ "${ACCESS_TOKEN}" == "" ] ; then
     exit -1
 fi
 
+# Start docker
+service docker start || exit -1
+
 # Get the token
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
